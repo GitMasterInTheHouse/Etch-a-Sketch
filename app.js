5c8d88324da1481469dc3716b3fd299c97666ca0
webdev <n3886628@gmail.com>
2025-11-08 14:04:30 +0100

    Finish generateGrid() function
    Finished the generateGrid() function. This function
    generates a grid when called.

---
+++ app.js
@@ -0,0 +1,17 @@
+const container = document.querySelector("#container");
+let gridSize = 16;
+
+function generateGrid(numOfCols) {
+  for (let i = 0; i < numOfCols; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < numOfCols; j++) {
+      const col = document.createElement("div");
+      col.classList.add("column");
+      row.appendChild(col);
+    }
+    container.appendChild(row);
+  }
+}
+
+generateGrid(gridSize);
