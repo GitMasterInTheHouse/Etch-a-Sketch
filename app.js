e9ca813a60c5f64d0b1a265a0e2ee6e4f6468369
webdev <n3886628@gmail.com>
2025-11-08 15:17:17 +0100

    Create the "scaling" functionality
    Allowed the grid to scale based on user input.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const container = document.querySelector("#container");
+const gridBtn = document.querySelector("#grid-btn");
 let gridSize = 16;
 
 function generateGrid(numOfCols) {
   for (let i = 0; i < numOfCols; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
+    row.style.width = container.style.width;
+    row.style.height = container.style.width / numOfCols;
     for (let j = 0; j < numOfCols; j++) {
       const col = document.createElement("div");
       col.classList.add("column");
+      col.style.width = row.style.width / numOfCols;
+      col.style.height = row.style.height;
       row.appendChild(col);
     }
     container.appendChild(row);
@@ -23,5 +28,23 @@ function draw() {
   });
 }
 
-generateGrid(gridSize);
-draw();
+const warningPara = document.createElement("p");
+document.querySelector("body").appendChild(warningPara);
+
+gridBtn.addEventListener("click", (e) => {
+  gridSize = prompt("Enter grid size:");
+  if (gridSize >= 100) {
+    warningPara.textContent = "Can't generate grid that large";
+  } else if (gridSize) {
+    if (document.querySelector(".column")) {
+      const rows = document.querySelectorAll(".row");
+      rows.forEach((row) => {
+        row.remove();
+      });
+    }
+    generateGrid(Number(gridSize));
+    draw();
+  } else {
+    warningPara.textContent = "Failed to generate grid";
+  }
+});
